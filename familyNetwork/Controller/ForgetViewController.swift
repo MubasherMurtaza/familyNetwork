d787ccbd7bc7e18f3e8f47aad5f5e4cc62f01c71
Mubasher Murtaza <mubashermurtaza@gmail.com>
2018-09-27 17:08:38 +0500

    add Tab

---
+++ familyNetwork/Controller/ForgetViewController.swift
@@ -0,0 +1,37 @@
+//
+//  ForgetViewController.swift
+//  familyNetwork
+//
+//  Created by Apple store on 27/09/2018.
+//  Copyright © 2018 MubasherMurtaza. All rights reserved.
+//
+
+import UIKit
+
+class ForgetViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+    override var preferredStatusBarStyle: UIStatusBarStyle {
+        return .lightContent
+    }
+    
+    @IBAction func backBtn(_ sender: UIButton) {
+        self.navigationController?.popViewController(animated: true)
+    }
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
